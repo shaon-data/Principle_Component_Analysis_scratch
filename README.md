9987359a6fc8112110e2f915730d24a5f7e72531
Shaon Majumder <shaon.fdr@robist.com>
2018-07-08 04:24:47 +0600

    separated from data analysis

---
+++ README.md
@@ -0,0 +1,4 @@
+# principle_component_analysis 
+## Download 
+For downloading use 
+       `git clone https://github.com/ShaonMajumder/principle_component_analysis.git` 
---
+++ pca.py
@@ -0,0 +1,59 @@
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+
+def covarience_matrix(X):
+    #standardizing data
+    X_std = StandardScaler().fit_transform(X)
+
+    #sample means of feature columns' of dataset
+    mean_vec = np.mean(X_std, axis=0) 
+    #covariance matrix
+    cov_mat = (X_std - mean_vec).T.dot((X_std - mean_vec)) / (X_std.shape[0]-1)
+    #if right handside is ( Xstd - mean(Xstd) )^T . ( Xstd - mean(Xstd) )
+    #simplyfying X^T.X / ( n - 1 )
+    cov_mat = np.cov(X_std.T)
+    return cov_mat
+
+
+
+def eign_decomposition(cov_mat):
+    #eign_decomposition
+    eign_vals, eign_vecs = np.linalg.eig(cov_mat)
+
+    print('Eigenvalues \n%s' %eign_vals)
+    print('\nEigenvectors \n%s' %eign_vecs)
+
+    
+    #for i in range(len(eig_vals)):
+    #    (np.abs(eig_vals[i]), eig_vecs[:,i] )
+    #make a list of (eigenvalue, eigenvector) tuples
+    eign_pairs = [ (np.abs(eign_vals[i]), eign_vecs[:,i] ) for i in range(len(eign_vals)) ]
+    return eign_pairs
+
+
+#covariance_matrix
+cov_mat = covarience_matrix(data_features)
+
+#eign_decomposition
+eig_pairs = eign_decomposition(cov_mat)
+#Sorting the (eignvalue, eignvector) tuples from high to low
+eig_pairs.sort()
+eig_pairs.reverse()
+
+#you can select the principle components by eign values, which one can be dropped
+#visually confirming about the list is correctly sorted by decreasing eignvaluess
+print("Eigenvalues in descending order:")
+for i in eig_pairs:
+    print(i[0])
+    
+#projection_matrix of our concatenated top k eigen vectors
+matrix_w = np.hstack(( eig_pairs[0][1].reshape(4,1),
+                       eig_pairs[1][1].reshape(4,1),
+                       eig_pairs[2][1].reshape(4,1) ))
+
+print("Matrix W:\n", matrix_w)
+'''
+we can choose how many dimensions we want for our subspace by chosing that amount of eignvectors
+to construct our dxk dimensional eignvector matrix w. lastly we will use our projection matrix
+to transform our samples onto to the subspaces via a simple dot product operation.
+'''
